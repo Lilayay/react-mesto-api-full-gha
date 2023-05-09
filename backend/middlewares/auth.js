8f4a3225a12f653e75d8cf6826c671e1310df57c
Lila Ratnikova <enilily@ya.ru>
2023-05-09 13:22:51 +0600

    jwt

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken');
 const AuthorizedError = require('../errors/UnautorizedError');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new AuthorizedError('Необходима авторизация'));
+    return next(new AuthorizedError('Необходимо авторизироваться'));
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, '5cdd183194489560b0e6bfaf8a81541e');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new AuthorizedError('Необходима авторизация'));
+    return next(new AuthorizedError('Необходимо авторизироваться'));
   }
-
   req.user = payload;
   return next();
 };
